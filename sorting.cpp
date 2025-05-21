03d885afc639e23638f80157992cdfbcbaa7d3e7
Himanshu Gupta <hgguptahimanshu29@gmail.com>
2025-05-21 11:18:58 +0530

    Sorting

---
+++ sorting.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void selection_sort(int arr[] , int n){
+    for(int i=0;i<=n-2;i++){
+        int mini = i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void bubble_sort(int arr[] , int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+            int temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+            }
+        }
+    }
+}
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = current;
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    
+    // selection_sort(arr , n);
+    // bubble_sort(arr , n);
+    insertion_sort(arr , n);
+    
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
